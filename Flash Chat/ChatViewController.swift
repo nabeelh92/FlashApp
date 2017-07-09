e416600d9b0f8d569479cb41de058efe3fd5e918
nabeelh92 <nabeelhuq@gmail.com>
2017-07-08 20:44:12 -0700

    added db listner for messages in real time

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -13,7 +13,7 @@ import Firebase
 class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
     // Declare instance variables here
-
+    var messageArray : [Message] = [Message]()
     
     // We've pre-linked the IBOutlets
     @IBOutlet var heightConstraint: NSLayoutConstraint!
@@ -45,6 +45,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
         
         configureTableView()
+        retrieveMessages()
     }
 
     ///////////////////////////////////////////
@@ -58,8 +59,11 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
 
         let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
         
-        let messageArray = ["First", "Second", "Third"]
-        cell.messageBody.text = messageArray[indexPath.row]
+        cell.messageBody.text = messageArray[indexPath.row].messageBody
+        cell.senderUsername.text = messageArray[indexPath.row].sender
+        
+        cell.avatarImageView.image = UIImage(named: "egg")
+        
         return cell
         
     }
@@ -67,7 +71,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     //TODO: Declare numberOfRowsInSection here:
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return messageArray.count 
     }
     
     //TODO: Declare tableViewTapped here:
@@ -152,7 +156,30 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     //TODO: Create the retrieveMessages method here:
+    func retrieveMessages() {
     
+        let MessageDB = FIRDatabase.database().reference().child("Messages")
+        
+        MessageDB.observe(.childAdded, with: { (snapshot) in
+            
+            let snapshotValue = snapshot.value as! Dictionary<String, String>
+            
+            let text = snapshotValue["MessageBody"]!
+            let sender = snapshotValue["Sender"]!
+            
+            
+            var message = Message()
+            message.sender = sender
+            message.messageBody = text
+            
+            self.messageArray.append(message)
+            
+            self.configureTableView()
+            self.messageTableView.reloadData()
+            
+        })
+        
+    }
     
 
     
